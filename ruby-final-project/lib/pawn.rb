c885dc78ae9f64aef9fdb80505c061c2ad7474d5
Eadsger <150641010+Eadsger@users.noreply.github.com>
2024-02-05 21:16:42 -0300

    Initial commit: Create Pawn class with initial movement rules

---
+++ ruby-final-project/lib/pawn.rb
@@ -0,0 +1,15 @@
+class Pawn
+  attr_reader :color
+
+  def initialize(color)
+    @color = color
+  end
+
+  def to_s
+    color == :black ? "♙" : "♟"
+  end
+
+  def move_directions
+    [[0, 1]]
+  end
+end
